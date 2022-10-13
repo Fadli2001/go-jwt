19ed029e743d859df1a7395b56985fe718ed0314
fadli <fadlirahmanfauzan04@gmail.com>
2022-10-13 09:16:03 +0700

    manual header

--- main.go
+++ main.go
@@ -1,20 +1,39 @@
 package main
 
 import (
-	"net/http"
-
 	"github.com/gin-gonic/gin"
 )
 
-func main(){
+type authHeader struct{
+	AuthorizationHeader string `header:"Authorization"`
+}
+
+func main() {
 	router := gin.Default()
-	router.GET("/customer",func(ctx *gin.Context){
-		ctx.JSON(http.StatusOK,gin.H{
-			"message" : "customer",
-		})
+
+	router.GET("/customer",func(ctx *gin.Context) {
+		h := authHeader{}
+		err := ctx.ShouldBindHeader(&h)
+		if err != nil {
+			ctx.JSON(401,gin.H{
+				"message":"Unauthorized",
+			})
+			return
+		}
+		if h.AuthorizationHeader == "123" {
+			ctx.JSON(200,gin.H{
+				"message":"customer",
+			})	
+			return		
+		}
+		ctx.JSON(401,gin.H{
+			"message" : "Unauthorized",
+		}) 
 	})
+
 	err := router.Run(":8888")
 	if err != nil {
 		panic(err)
-	}
+	}	
+
 }
